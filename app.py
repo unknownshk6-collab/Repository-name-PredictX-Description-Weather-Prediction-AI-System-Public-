9c102bad6f0a8afeea76ebe62663b42b185fb2c8
unknownshk6-collab <unknownshk6@gmail.com>
2026-04-26 18:03:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print("PredictX is working")
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "PredictX API Running"
+
+@app.route('/predict')
+def predict():
+    temp = request.args.get('temp')
+    humidity = request.args.get('humidity')
+
+    if float(humidity) > 75:
+        result = "Rain 🌧️"
+    else:
+        result = "No Rain ☀️"
+
+    return jsonify({
+        "temperature": temp,
+        "humidity": humidity,
+        "prediction": result
+    })
+
+app.run()
